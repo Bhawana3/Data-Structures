41de93db0357bea5e251421898d5397043971ed6
bhawana <vermabhawana50@gmail.com>
2016-06-14 13:30:42 +0530

    added push node method.

--- linked_list.py
+++ linked_list.py
@@ -7,6 +7,7 @@ class LinkedList():
 	def __init__(self):
 		self.head = None		# head refers to empty linked list 
 	
+	""" append(insert node in the last) new node in the linked list"""
 	def append(self,value):			# input value of node
 		temp = Node(value)		# new node to be appended with node value equals to value 
 		current = self.head
@@ -17,7 +18,10 @@ class LinkedList():
 		else:
 			self.head = temp 
 	
-	def traverse(self):			# finds the length of list	
+	""" Find the length of the linked list 
+			or
+	 traverse through the linked list"""
+	def traverse(self):				
 		counter = 0 
 		current = self.head
 		if self.head is not None:	#finds if list is not empty
@@ -27,7 +31,9 @@ class LinkedList():
 			return counter
 		return counter
 	
-	def find_node_at_given_position(self,position):	# given position finds the value of node at that position
+	""" Find the value of node at 
+		given position"""
+	def find_node_at_given_position(self,position):	
 		current = self.head
 		counter = 0 
 		if self.head is not None:
@@ -41,8 +47,9 @@ class LinkedList():
 			else:
 				return None
 		else:
-			return None
-
+			return "No linked list exists" 
+	
+	""" prints out linked list"""
 	def print_linked_list(self):
 		current = self.head
 		if self.head is not None:
@@ -51,11 +58,13 @@ class LinkedList():
 				current = current.next
 		else:
 			"No linked list exists"				
-
-	def delete_last_element(self):
+	
+	""" Delete last node 
+		from the linked list"""
+	def delete_last_node(self):
 		current = self.head
-		next_element_of_current = current.next 
 		if self.head is not None:
+			next_element_of_current = current.next 
 			if current.next == None:
 				self.head = None
 			else:
@@ -65,6 +74,17 @@ class LinkedList():
 				current.next = None
 		else:
 			return "No linked list exists"	
+	
+	"""push a new node on the front
+   		 of the list."""
+	def push_new_node_in_front(self,value):			# push new node in front of the linked list
+		temp = Node(value)			# creates new node having reference temp
+		if self.head is not None:			# if linked list is not empty
+			temp.next = self.head			# temp points towards head
+			self.head = temp			# make head and temp equal
+		else:
+			self.head = temp			# when linked list is empty
+			temp.next = None		
 
 if __name__ == "__main__":
 	List = LinkedList()
@@ -74,7 +94,8 @@ if __name__ == "__main__":
 	print "3 - Find the node at position entered by user"
 	print "4 - Print linked list"
 	print "5 - Delete last element from linked list"
-	n = raw_input("Choose an option(1/2/3/4/5) :")
+	print "6 - Push new node in front of the linked list"
+	n = raw_input("Choose an option(1/2/3/4/5/6) :")
 	while True:
 		if n == '1':
 			val = input("Enter any value to append to the list:")
@@ -87,10 +108,13 @@ if __name__ == "__main__":
 		elif n == '4':
 			print List.print_linked_list()
 		elif n == '5':
-			print List.delete_last_element() 
+			print List.delete_last_node() 
+		elif n =='6':
+			val = input("Enter any value to push into the linked list:")
+			List.push_new_node_in_front(val)
 		else:
 			print "Invalid option choosed.Exiting ...."
 			break
-		n = raw_input("Choose an option(1/2/3/4/5): ")
+		n = raw_input("Choose an option(1/2/3/4/5/6): ")
 		
 		 
