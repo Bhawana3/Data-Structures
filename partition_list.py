be2d739269e1886523b0a0d958e6aaa48fe05135
bhawana <vermabhawana50@gmail.com>
2016-07-05 12:14:00 +0530

    added method to partition linked list from given value.

---
+++ partition_list.py
@@ -0,0 +1,98 @@
+class Node:
+	def __init__(self):
+		self.value = 0
+		self.next = None
+	
+class LinkedList:
+	def __init__(self):
+		self.head = None
+
+	def append(self,value):
+		temp = Node()
+		temp.value = value
+		current = self.head
+		if current is not None:
+			while current.next != None:
+				current = current.next
+			current.next = temp
+		else:
+			self.head = temp
+	
+	""" Insert node into front of list"""
+	def insert_into_front(self,value):
+		temp = Node()
+		temp.value = value
+		current = self.head
+		if current is not None:
+			temp.next = current
+			self.head = temp
+		else:
+			self.head = temp
+	
+	def printList(self):
+		current = self.head
+		if current is not None:
+			while current != None:
+				print current.value ,"-->",
+				current = current.next
+			print None
+		else:
+			print "Empty Linked list"
+	
+	""" partition linked list from the given value into smaller
+	 in left side and larger in right side of the element"""
+
+	def partition(self,value):
+		smaller_list = LinkedList()
+		larger_list = LinkedList()
+		new_list = LinkedList()
+		current = self.head
+		if current is not None:
+			while current != None:
+				if current.value < value:
+					temp = current.value
+					smaller_list.append(temp)
+				elif current.value == value:
+					temp = current.value
+					larger_list.insert_into_front(temp)
+				else:
+					temp = current.value
+					larger_list.append(temp)
+				current = current.next
+			small = smaller_list.head		
+			if small is not None:
+				while small.next != None:
+					small = small.next
+				small.next = larger_list.head		# merge smaller list with larger list
+				new_list.head = smaller_list.head
+			else:
+				new_list.head = larger_list.head
+			return new_list.printList()			# merge both lists into new list
+		else:
+			print "Empty linked list"
+
+if __name__ == "__main__":
+	Llist = LinkedList()
+	print "Menu :"
+	print "1 - append into list"
+	print "2 - insert into front of list"
+	print "3 - print list"
+	print "4 - partition list"
+	n = raw_input("Choose from above option : ")
+	while True:
+		if n == '1':
+			value = input("Enter value to node : ")
+			Llist.append(value)
+		elif n == '2':
+			value = input("Enter value to node : ")
+			Llist.insert_into_front(value)
+		elif n == '3':
+			Llist.printList()
+		elif n == '4':
+			value = input("Enter value to find in the list : ")
+			Llist.partition(value)
+		else:
+			print "Invalid option choosen.Exiting.."
+			break
+		n = raw_input("Choose from above option : ")
+
